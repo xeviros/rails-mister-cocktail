ae117daec25b46e6bd6120fc9b466e56d5872805
Xavier Ros <x.ros.martinez@gmail.com>
2018-06-07 15:40:28 +0200

    cocktail working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Sex on the Beach")
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
